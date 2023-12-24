fa04b2fcf9423a1c687343d61a8f14e54f4b3ed1
Benzazoua <billalben252@gmail.com>
2023-12-24 11:25:09 +0100

    Preloader

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+window.addEventListener("load", () => {
+  const preloader = document.querySelector("[data-preloader]");
+  setTimeout(() => {
+    preloader.classList.add("remove");
+  }, 1000);
+});
