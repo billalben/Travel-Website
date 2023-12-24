73c6a3d53f6df7246099287169b5e2856788e1d6
Benzazoua <billalben252@gmail.com>
2023-12-24 17:20:42 +0100

    Header
    - header structure
    - header style
    - header js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,30 @@ window.addEventListener("load", () => {
     preloader.classList.add("remove");
   }, 1000);
 });
+
+// Add event on multiple elements
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const item of elements) {
+    item.addEventListener(eventType, callback);
+  }
+};
+
+// Navbar toggler for mobile
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navToggler, "click", toggleNav);
+
+// Header
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  header.classList[this.scrollY >= 120 ? "add" : "remove"]("active");
+});
